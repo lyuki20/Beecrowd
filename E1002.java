1e3ea9c25005767a80db8bf1b9b218841175e7c3
Yuki Tsutsumi <lucayuki20@gmail.com>
2022-09-03 17:33:27 -0300

    1002

---
+++ E1002.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class E1002 {
+    public static void main(String[] args) throws IOException {
+
+        Scanner in = new Scanner(System.in);
+        double r = in.nextDouble();
+        r = r * r;
+        double pi = 3.14159;
+        System.out.println(String.format("A=%.4f", pi * r));
+
+    }
+
+}
+
+}
